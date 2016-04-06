13c8d34505722020d51bbd366c98fe8e826389f1
Mateusz Kuzło <kuzlo.mateusz@gmail.com>
2016-04-06 18:25:34 +0200

    Dodano zawartość biblioteki GALib

---
+++ src/main/java/edgeColoringGA/GALib/GAException.java
@@ -0,0 +1,17 @@
+package edgeColoringGA.GALib;
+
+/**
+ * Custom GA exception class
+ * @author Jeff Smith jeff@SoftTechDesign.com
+ */
+public class GAException extends Exception
+{
+    /**
+     * GAException constructor
+     * @param msg
+     */
+    GAException(String msg)
+    {
+        super(msg);
+    }
+}
